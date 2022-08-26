aba366bf36fce4f32c8e6b80d2b42ff7702ab9ef
Ashish Raj <107697972+Ashish0RAJ@users.noreply.github.com>
2022-08-26 19:29:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ var playicon = document.getElementById("play");
 var song = document.getElementById("mysong");
 
 playicon.onclick = function () {
-    song.play();
+    if (song.paused) {
+        song.play();
+        playicon.classList.remove("fa-play");
+        playicon.classList.add("fa-pause");
+    } else {
+        song.pause();
+        playicon.classList.remove("fa-pause");
+        if (playicon.classList != ("fa-play")) {
+            playicon.classList.add("fa-play");
+        }
+    }
 }
